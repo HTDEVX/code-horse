0d9a76e251dffcfdd01269c30156514e166c94aa
HTDEVX <hirendratiwrekar93@gmail.com>
2026-03-20 17:29:17 +0530

    integrated prismaORM | neon db

---
+++ prisma/migrations/20260320115506_test/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Test_email_key" ON "Test"("email");
